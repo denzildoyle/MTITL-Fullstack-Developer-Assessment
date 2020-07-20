521ab06b6c8d46cdf0abd2f1456297f0e8c8844c
Denzil Doyle <denzildoyle@gmail.com>
2020-07-20 03:15:05 -0400

    Read data from file to test getMaximumSum

--- GetMaximumSum/getMaximumSum.js
+++ GetMaximumSum/getMaximumSum.js
@@ -1,8 +1,9 @@
 function getMaximumSum(triangle) {
-  for (let x = triangle.length - 2; x >= 0; x--)
-    for (let i = 0; i <= x; i++)
-      triangle[x][i]+= Math.max(triangle[x + 1][i], triangle[x + 1][i + 1]);
-  return triangle[0][0];
+    for (let x = triangle.length - 2; x >= 0; x--)
+        for (let i = 0; i <= x; i++)
+            triangle[x][i] = parseInt(triangle[x][i]) + Math.max(parseInt(triangle[x + 1][i]), parseInt(triangle[x + 1][i + 1]));
+
+    return triangle[0][0];
 }
 
 module.exports = getMaximumSum;
--- GetMaximumSum/getMaximumSum.test.js
+++ GetMaximumSum/getMaximumSum.test.js
@@ -1,14 +1,36 @@
 const getMaximumSum = require('./getMaximumSum');
 
-const triangle1 = [
+const triangle = [
   [5],
   [9, 6],
   [4, 6, 8],
   [0, 7, 1, 5]
 ];
 
+const readline = require('readline');
+const fs = require('fs');
+
+let rows = 0;
+let triangleFile = new Array();
+
+const rl = readline.createInterface({
+  input: fs.createReadStream('triangle.txt')
+});
+
+rl.on('line', async(line) => {
+    triangleFile[rows] = line.split(' ');
+    rows++;
+});
+
 describe('MAXIMUM SUM', () => {
     test('Total equal 27', () => {
-        expect(getMaximumSum(triangle1)).toBe(27);
+        expect(getMaximumSum(triangle)).toBe(27);
+    });
+
+    rl.on('close', (line) => {
+        test('Total in File equal 732506', async () => {
+            const triangleFile = await fetchData();
+            expect(getMaximumSum(triangleFile)).toBe(732506);
+        });
     });
 });
