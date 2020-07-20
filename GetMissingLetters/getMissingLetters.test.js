4ef152b2d34ba4f1ec6516268e7a8a57c50a2be3
Denzil Doyle <denzildoyle@gmail.com>
2020-07-20 01:21:44 -0400

    Refactor getMissingLetters test cases

--- GetMissingLetters/getMissingLetters.test.js
+++ GetMissingLetters/getMissingLetters.test.js
@@ -4,14 +4,16 @@ const oneLetterMissing = new MissingLetters("abcdefghijklmnopqrstuvwxy");
 const allLetters = new MissingLetters("abcdefghijklmnopqrstuvwxyz");
 const sentanceWithAllLetters = new MissingLetters("A quick brown fox jumps over the lazy dog");
 
-test('Z missing', () => {
-    expect(oneLetterMissing.GetMissingLetters).toBe('z');
-});
+describe('MISSING LETTERS', () => {
+    test('Z missing', () => {
+        expect(oneLetterMissing.GetMissingLetters).toBe('z');
+    });
 
-test('All letters', () => {
-    expect(allLetters.GetMissingLetters).toBe('');
-});
+    test('All letters', () => {
+        expect(allLetters.GetMissingLetters).toBe('');
+    });
 
-test('Sentance with all letters', () => {
-    expect(allLetters.GetMissingLetters).toBe('');
+    test('Sentance with all letters', () => {
+        expect(allLetters.GetMissingLetters).toBe('');
+    });
 });
